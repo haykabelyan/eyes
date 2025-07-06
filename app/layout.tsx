3531dad4a01630a3c127309c40f014a0060b5e18
haykabelyan <haykabelyan1@gmail.com>
2025-07-06 23:43:48 +0400

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MyHeader } from "@/components/Header";
 import { MyFooter } from "@/components/Footer";
